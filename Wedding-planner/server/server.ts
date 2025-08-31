7d8f5b7e6e9e40e98b3f27e6ad2b89500bdfeaf1
Galit97 <galit.liou@gmail.com>
2025-08-31 20:19:27 +0300

    u

--- Wedding-planner/server/server.ts
+++ Wedding-planner/server/server.ts
@@ -54,7 +54,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {});
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err: any) => {
     console.error("Failed to start server:", err);
